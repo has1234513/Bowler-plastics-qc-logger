7482b2b1e9927daccdc4fd7335b174f176ff97e5
has1234513 <hashaydar29@gmail.com>
2026-05-20 02:22:45 +0200

    Define fillable attributes and casts for EnvironmentalLog model

--- app/Models/EnvironmentalLog.php
+++ app/Models/EnvironmentalLog.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class EnvironmentalLog extends Model
 {
-    //
+    protected $fillable = [
+        'batch_number',
+        'temperature_celsius',
+        'humidity_percent',
+        'logged_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'logged_at' => 'datetime',
+            'temperature_celsius' => 'decimal:2',
+            'humidity_percent' => 'integer',
+        ];
+    }
 }
