b4c291e553a25d59414860df2f49c5f26890b547
has1234513 <hashaydar29@gmail.com>
2026-05-20 03:27:08 +0200

    Add link to QC Dashboard in Welcome component

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -125,6 +125,14 @@ export default function Welcome({
                             </ul>
                             <ul className="flex gap-3 text-sm leading-normal">
                                 <li>
+                                    <Link
+                                        href="/qc"
+                                        className="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white"
+                                    >
+                                        Go to QC Dashboard
+                                    </Link>
+                                </li>
+                                <li>
                                     <a
                                         href="https://cloud.laravel.com"
                                         target="_blank"
